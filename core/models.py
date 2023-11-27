cbc263627fee8315115a1c9bd5eda607b86ab641
NeitUSM <141852352+NeitUSM@users.noreply.github.com>
2023-11-27 12:41:43 -0300

    Issue #2
    Problema #2 resuelto.

--- core/models.py
+++ core/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+
+class Evento(models.Model):
+    fecha_inicio = models.DateField()
+    fecha_termino = models.DateField()
+    titulo = models.CharField(max_length=255)
+    descripcion = models.TextField()
+    tipo = models.CharField(max_length=255)
+    segmentos = models.ManyToManyField('Segmento')
+
+
+class Segmento(models.Model):
+    nombre = models.CharField(max_length=255)
