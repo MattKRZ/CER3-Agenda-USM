9fdbe4a1a5e211940712bd8ee48213bff73dfc2f
NeitUSM <141852352+NeitUSM@users.noreply.github.com>
2023-11-27 16:15:30 -0300

    Issue #4 #5 #16
    Problemas resueltos (4 , 5 , 16)

--- core/admin.py
+++ core/admin.py
@@ -1,11 +1,7 @@
 from django.contrib import admin
-from .models import Evento, Segmento
+from .models import Evento
 
-# Register your models here.
+
 @admin.register(Evento)
 class EventoAdmin(admin.ModelAdmin):
     list_display = ('titulo', 'fecha_inicio', 'fecha_termino', 'tipo',)
-
-@admin.register(Segmento)
-class SegmentoAdmin(admin.ModelAdmin):
-    list_display = ('nombre',)
